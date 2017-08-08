5645d7c1293650abb47975ed3b5296a4f9564d64
RAVI VERMA <ravi.verma278@gmail.com>
2017-08-08 15:12:52 +0530

    Pojo class
    Pojo class for topics

---
+++ Topic.java
@@ -0,0 +1,64 @@
+    package com.db.springbootcouchbase.domain;
+
+import com.couchbase.client.java.repository.annotation.Field;
+import com.couchbase.client.java.repository.annotation.Id;
+import org.springframework.data.couchbase.core.mapping.Document;
+
+/**
+ * Created by RV00451128 on 7/17/2017.
+ */
+@Document
+public class Topic {
+
+    @Id
+    private String id;
+
+    @Field
+    private String name;
+
+    @Field
+    private String description;
+
+    public Topic(){
+
+    }
+    public Topic(String id, String name, String description) {
+        super();
+        this.id = id;
+        this.name = name;
+        this.description = description;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    @Override
+    public String toString() {
+        return "Topic{" +
+                "id='" + id + '\'' +
+                ", name='" + name + '\'' +
+                ", description='" + description + '\'' +
+                '}';
+    }
+}
